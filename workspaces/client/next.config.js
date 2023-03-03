df38a7ab9f634dc3865b04f5a43be81d02954fcc
happy <happy@example.com>
2023-03-03 13:35:31 -0800

    happyDev

--- workspaces/client/next.config.js
+++ workspaces/client/next.config.js
@@ -1,6 +1,6 @@
 // Need to transpile typescript from shared workspace
 const withTM = require('next-transpile-modules')([
-  // '@memory-cards/shared',
+  '@memory-cards/shared',
 ]);
 
 /** @type {import('next').NextConfig} */
